680e5123ca549bb1e72a286717842c8d789b988a
stormytuna <stormytuna@outlook.com>
2022-12-24 20:51:04 +0000

    Add magma canister

---
+++ Content/Items/Canisters/MagmaCanister.cs
@@ -0,0 +1,55 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Canisters.Content.Items.Canisters {
+    public class MagmaCanister : CanisterItem {
+        public override void SetStaticDefaults() {
+            SacrificeTotal = 99;
+
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults() {
+            // Base stats
+            Item.width = 22;
+            Item.height = 22;
+            Item.maxStack = 999;
+            Item.value = 2;
+
+            // Use stats
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = Item.useAnimation = 20;
+            Item.autoReuse = true;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.consumable = true;
+
+            // Weapon stats
+            Item.shoot = ModContent.ProjectileType<Projectiles.Canisters.MagmaCanister>();
+            Item.shootSpeed = 9f;
+            Item.damage = 1;
+            Item.crit = 4;
+            Item.knockBack = 8f;
+            Item.DamageType = DamageClass.Ranged;
+
+            // CanisterItem stats
+            LaunchedProjectileType = Item.shoot;
+            DepletedProjectileType = -1;
+            DamageWhenLaunched = 14;
+            DamageWhenDepleted = 3;
+
+            base.SetDefaults();
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe(150)
+                .AddIngredient<EmptyCanister>(150)
+                .AddTile(TileID.Bottles)
+                .AddCondition(Recipe.Condition.NearLava)
+                .Register();
+
+            base.AddRecipes();
+        }
+    }
+}
---
+++ Content/Projectiles/Canisters/MagmaCanister.cs
@@ -0,0 +1,88 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Canisters.Content.Projectiles.Canisters {
+    public class MagmaCanister : ModProjectile {
+        public override void SetDefaults() {
+            // Base stats
+            Projectile.width = 22;
+            Projectile.height = 22;
+            Projectile.aiStyle = 2;
+
+            // Weapon stats
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.DamageType = DamageClass.Ranged;
+
+            base.SetDefaults();
+        }
+
+        public override string Texture => "Canisters/Content/Items/Canisters/MagmaCanister";
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+            if (Projectile.alpha != 255) {
+                Explode();
+            }
+
+            if (Main.rand.NextBool(3)) {
+                target.AddBuff(BuffID.OnFire, 180);
+            }
+
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) {
+            if (Projectile.alpha != 255) {
+                Explode();
+                return false;
+            }
+
+            return base.OnTileCollide(oldVelocity);
+        }
+
+        private void Explode() {
+            SoundEngine.PlaySound(SoundID.DD2_GoblinBomb, Projectile.Center);
+
+            for (int i = 0; i < 80; i++) {
+                // Our base dust properties
+                Vector2 velocity = Main.rand.NextVector2Circular(15f, 15f);
+                Color color = Main.rand.NextBool(10) ? Color.Red : default;
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, newColor: color, Scale: Main.rand.NextFloat(0.8f, 1.2f));
+                dust.velocity = velocity;
+                dust.noGravity = true;
+                dust.noLight = true;
+
+                if (Main.rand.NextBool(6)) {
+                    // 1/7 dust become large, slow dust
+                    float sizeMult = Main.rand.NextFloat(3f, 4f);
+                    dust.scale *= sizeMult;
+                    dust.velocity /= sizeMult;
+                } else if (Main.rand.NextBool(3)) {
+                    // 1/4 dust becomes medium dust
+                    float sizeMult = Main.rand.NextFloat(2f, 3f);
+                    dust.scale *= sizeMult;
+                    dust.velocity /= sizeMult;
+                } else {
+                    // Rest of dust are little sparks that are gravity affected
+                    dust.velocity.X /= 4f;
+                    dust.velocity.Y = MathF.Abs(dust.velocity.Y) / -4f;
+                    dust.noGravity = false;
+                }
+            }
+
+            Projectile.TurnToExplosion(96, 96);
+
+            // Smoke dust
+            for (int i = 0; i < 20; i++) {
+                Vector2 velocity = Main.rand.NextVector2Circular(5f, 5f);
+                Dust dust = Dust.NewDustDirect(Projectile.position, 96, 96, DustID.Smoke, Scale: Main.rand.NextFloat(1f, 1.5f));
+                dust.velocity = velocity;
+                dust.noGravity = true;
+            }
+        }
+    }
+}
