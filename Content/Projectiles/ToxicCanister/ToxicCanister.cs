42637b8b68c96e91aa4c7d43e1d20fc59a31453c
stormytuna <stormytuna@outlook.com>
2023-09-23 19:22:10 +0100

    Add toxic canister outline

--- Content/Projectiles/ToxicCanister/ToxicCanister.cs
+++ Content/Projectiles/ToxicCanister/ToxicCanister.cs
@@ -1,5 +1,8 @@
-﻿using Canisters.Helpers;
+﻿using System.IO;
+using Canisters.Helpers;
 using Canisters.Helpers.Abstracts;
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
 
@@ -8,14 +11,46 @@ namespace Canisters.Content.Projectiles.ToxicCanister;
 /// <summary>
 ///     Vial of venom canister
 /// </summary>
-// TODO: Actually make this
+// TODO: Visuals, balancing
 public class ToxicCanister : CanisterProjectile
 {
 	public override string Texture => "Canisters/Content/Items/Canisters/ToxicCanister";
 
+	private bool hasExploded;
+
+	public override void AI() {
+		if (!hasExploded) {
+			return;
+		}
+
+		Projectile.aiStyle = -1;
+		Projectile.velocity = Vector2.Zero;
+
+		for (int i = 0; i < 10; i++) {
+			Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.DemonTorch);
+			d.noGravity = true;
+			d.noLight = true;
+			d.noLightEmittence = true;
+		}
+	}
+
 	public override void Explode() {
 		SoundEngine.PlaySound(SoundID.DD2_GoblinBomb, Projectile.Center);
 
-		Projectile.TurnToExplosion(96, 96);
+		Projectile.TurnToExplosion(200, 200);
+		Projectile.knockBack = 0f;
+		Projectile.timeLeft = 200;
+		hasExploded = true;
+		Projectile.netUpdate = true;
+
+		// TODO: Dust explosion
+	}
+
+	public override void SendExtraAI(BinaryWriter writer) {
+		writer.Write(hasExploded);
+	}
+
+	public override void ReceiveExtraAI(BinaryReader reader) {
+		hasExploded = reader.ReadBoolean();
 	}
 }
