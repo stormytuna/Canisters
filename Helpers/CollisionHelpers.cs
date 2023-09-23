242bd01b7c90572a9898717748b41f45d0e16e7f
stormytuna <stormytuna@outlook.com>
2023-09-23 19:53:23 +0100

    Update CollisionHelpers.cs

--- Helpers/CollisionHelpers.cs
+++ Helpers/CollisionHelpers.cs
@@ -5,7 +5,7 @@ namespace Canisters.Helpers;
 
 public static class CollisionHelpers
 {
-	public static bool CanHit(Entity source, Vector2 targetPos, int targetWidth = 1, int targetHeight = 1) => Collision.CanHit(source.position, source.width, source.height, targetPos, targetWidth, targetHeight);
+	public static bool CanHit(Entity source, Vector2 targetPos, int targetWidth = 0, int targetHeight = 0) => Collision.CanHit(source.position, source.width, source.height, targetPos, targetWidth, targetHeight);
 
 	public static bool CanHit(Vector2 source, Vector2 target) => Collision.CanHit(source, 0, 0, target, 0, 0);
 }
