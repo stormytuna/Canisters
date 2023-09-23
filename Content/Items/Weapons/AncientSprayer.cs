1574b00a5bfa90593227591ad16666abd1a555d0
stormytuna <stormytuna@outlook.com>
2023-09-23 23:16:38 +0100

    Add Ancient Sprayer backpack

--- Content/Items/Weapons/AncientSprayer.cs
+++ Content/Items/Weapons/AncientSprayer.cs
@@ -1,7 +1,11 @@
 ﻿using Canisters.Content.Projectiles.VolatileCanister;
 using Canisters.Helpers;
 using Canisters.Helpers.Abstracts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -41,5 +45,30 @@ public class AncientSprayer : CanisterUsingWeapon
 			.AddRecipeGroup(RecipeGroupID.IronBar, 10)
 			.AddTile(TileID.Anvils)
 			.Register();
+	}
+}
+
+// TODO: Visuals for this
+public class AncientSprayerDrawLayer : PlayerDrawLayer
+{
+	private Asset<Texture2D> _texture;
+	private Asset<Texture2D> Texture => _texture ??= ModContent.Request<Texture2D>("Canisters/Content/Items/Weapons/AncientSprayer_Back");
+
+	public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.Backpacks);
+
+	public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => drawInfo.drawPlayer.HeldItem.type == ModContent.ItemType<AncientSprayer>();
+
+	protected override void Draw(ref PlayerDrawSet drawInfo) {
+		Player drawPlayer = drawInfo.drawPlayer;
+
+		Vector2 drawPosition = drawInfo.Position - Main.screenPosition + new Vector2(drawPlayer.width / 2, drawPlayer.height - drawPlayer.bodyFrame.Height / 2);
+		drawPosition.X += drawPlayer.direction * -13f;
+		drawPosition.Y += drawPlayer.gravDir * 2f;
+		drawPosition = drawPosition.Floor();
+		Rectangle sourceRect = new(0, 0, Texture.Width(), Texture.Height());
+		Vector2 origin = sourceRect.Size() / 2f;
+
+		DrawData drawData = new(Texture.Value, drawPosition, sourceRect, drawInfo.colorArmorBody, drawPlayer.bodyRotation, origin, 1f, drawInfo.playerEffect);
+		drawInfo.DrawDataCache.Add(drawData);
 	}
 }
