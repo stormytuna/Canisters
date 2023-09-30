86a72a778e9fe7e882f7233d175efe075a68514e
stormytuna <stormytuna@outlook.com>
2023-09-30 22:11:13 +0100

    Add some dust to Devoured debuff

--- Content/Buffs/Devoured.cs
+++ Content/Buffs/Devoured.cs
@@ -52,7 +52,7 @@ public class DevouredGlobalNPC : GlobalNPC
 	}
 
 	public override void OnKill(NPC npc) {
-		if (Main.netMode != NetmodeID.MultiplayerClient && Devoured) {
+		if (Devoured) {
 			Vector2 velocity = Main.rand.NextVector2CircularEdge(5f, 5f) * Main.rand.NextFloat(0.5f, 1f);
 			int damage = NaniteBaseDamage + (int)MathF.Ceiling(naniteBonusDamage);
 			Projectile.NewProjectile(npc.GetSource_Death(), npc.Center, velocity, ModContent.ProjectileType<Nanites>(), damage, 2f, npc.lastInteraction);
@@ -60,8 +60,14 @@ public class DevouredGlobalNPC : GlobalNPC
 	}
 
 	public override void DrawEffects(NPC npc, ref Color drawColor) {
-		if (Devoured) {
-			drawColor = Color.Lerp(drawColor, Color.Red, 0.5f);
+		if (!Devoured) {
+			return;
+		}
+
+		if (Main.rand.NextBool(3)) {
+			Dust dust = Dust.NewDustDirect(npc.position, npc.width, npc.height, DustID.Clentaminator_Cyan);
+			dust.noGravity = true;
+			dust.velocity *= 1f;
 		}
 	}
 }
