5ac535ab86adf55ed93a9ba03b78b8400eeebc6f
stormytuna <stormytuna@outlook.com>
2023-09-30 22:40:15 +0100

    Add toxic fog

---
+++ Content/Projectiles/ToxicCanister/ToxicFog.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Canisters.Content.Projectiles.ToxicCanister;
+
+public class ToxicFog : ModProjectile
+{
+	public override void SetStaticDefaults() {
+		Main.projFrames[Type] = 3;
+	}
+
+	public override void SetDefaults() {
+		// Base stats
+		Projectile.width = 60;
+		Projectile.height = 60;
+		Projectile.aiStyle = -1;
+		Projectile.alpha = 40;
+		Projectile.penetrate = -1;
+		Projectile.alpha = 120;
+
+		// First frame
+		Projectile.frame = Main.rand.Next(Main.projFrames[Type]);
+	}
+
+	public override void AI() {
+		// Fade out
+		Projectile.alpha++;
+		if (Projectile.alpha >= 255) {
+			Projectile.Kill();
+		}
+
+		// Scale up
+		Projectile.scale += 0.01f;
+
+		// Velocity slowdown
+		Projectile.velocity *= 0.99f;
+
+		// Animate
+		Projectile.frameCounter++;
+		if (Projectile.frameCounter >= 12) {
+			Projectile.frameCounter = 0;
+			Projectile.frame++;
+			if (Projectile.frame >= Main.projFrames[Type]) {
+				Projectile.frame = 0;
+			}
+		}
+	}
+
+	public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac) {
+		width = 20;
+		height = 20;
+
+		return base.TileCollideStyle(ref width, ref height, ref fallThrough, ref hitboxCenterFrac);
+	}
+}
--- Content/Projectiles/ToxicCanister/ToxicFogEmitter.cs
+++ Content/Projectiles/ToxicCanister/ToxicFogEmitter.cs
@@ -1,4 +1,5 @@
 ﻿using Canisters.Helpers;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,20 +12,32 @@ public class ToxicFogEmitter : ModProjectile
 
 	public override void SetDefaults() {
 		// Base stats
-		Projectile.width = Projectile.height = 200;
+		Projectile.width = Projectile.height = 100;
 		Projectile.aiStyle = -1;
 		Projectile.tileCollide = false;
 		Projectile.timeLeft = 200;
 		Projectile.penetrate = -1;
 	}
 
+	private ref float Timer => ref Projectile.ai[0];
+
 	public override void AI() {
-		// TODO: Actually emit toxic fog
-		for (int i = 0; i < 10; i++) {
-			Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.DemonTorch);
+		if (Timer % 10 == 0 && Timer <= 140f && Projectile.owner == Main.myPlayer) {
+			Vector2 offset = Main.rand.NextVector2Circular(50f, 50f);
+			Vector2 spawnPosition = Projectile.Center + offset;
+			Vector2 spawnVelocity = Main.rand.NextVector2Circular(0.5f, 0.5f);
+			Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), spawnPosition, spawnVelocity, ModContent.ProjectileType<ToxicFog>(), 0, 0f, Projectile.owner);
+		}
+
+		for (int i = 0; i < 2; i++) {
+			Vector2 offset = Main.rand.NextVector2Circular(50f, 50f);
+			Vector2 spawnPosition = Projectile.Center + offset;
+			Dust d = Dust.NewDustPerfect(spawnPosition, DustID.DemonTorch);
 			d.noGravity = true;
 			d.noLight = true;
 			d.noLightEmittence = true;
 		}
+
+		Timer++;
 	}
 }
