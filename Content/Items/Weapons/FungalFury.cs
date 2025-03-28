c8d49914c152f469e3830d45b332286de4c4eb84
stormytuna <stormytuna@outlook.com>
2025-03-28 19:37:13 +0000

    Add Fungal Fury sprites and skeleton class

---
+++ Content/Items/Weapons/FungalFury.cs
@@ -0,0 +1,45 @@
+﻿using Canisters.DataStructures;
+using Canisters.Helpers;
+using Canisters.Helpers.Enums;
+using ReLogic.Content;
+using Terraria.DataStructures;
+using Terraria.Enums;
+
+namespace Canisters.Content.Items.Weapons;
+
+public class FungalFury : BaseCanisterUsingWeapon
+{
+	public override CanisterFiringType CanisterFiringType {
+		get => CanisterFiringType.Launched;
+	}
+
+	public override Vector2 MuzzleOffset {
+		get => new(42f, -2f);
+	}
+
+	public override Vector2? HoldoutOffset() {
+		return new Vector2(-4f, 0f);
+	}
+
+	public override void SetDefaults() {
+		Item.DefaultToCanisterUsingWeapon(10, 10, 9f, 11, 1f);
+		Item.width = 60;
+		Item.height = 32;
+		Item.SetShopValues(ItemRarityColor.Blue1, Item.buyPrice(silver: 40));
+	}
+
+	public override void ApplyWeaponStats(ref CanisterShootStats stats) {
+		stats.Velocity = stats.Velocity.RotatedByRandom(0.4f);
+	}
+}
+
+public class FungalFuryGlobalNPC : GlobalNPC
+{
+	public override bool AppliesToEntity(NPC entity, bool lateInstantiation) {
+		return entity.type == NPCID.Truffle;
+	}
+
+	public override void ModifyShop(NPCShop shop) {
+		shop.InsertAfter(ItemID.MushroomSpear, ModContent.ItemType<FungalFury>());
+	}
+}
