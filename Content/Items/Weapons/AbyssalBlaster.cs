d22828ddd9c4cce182511a3d4b1787962aed8a5d
stormytuna <stormytuna@outlook.com>
2025-05-19 18:34:26 +0100

    Add abyssal blaster

---
+++ Content/Items/Weapons/AbyssalBlaster.cs
@@ -0,0 +1,37 @@
+using Canisters.DataStructures;
+using Canisters.Helpers;
+using Canisters.Helpers.Enums;
+using Terraria.Enums;
+
+namespace Canisters.Content.Items.Weapons;
+
+public class AbyssalBlaster : BaseCanisterUsingWeapon
+{
+	public override CanisterFiringType CanisterFiringType {
+		get => CanisterFiringType.Launched;
+	}
+
+	public override Vector2 MuzzleOffset {
+		get => new(20f, 0f);
+	}
+
+	public override Vector2? HoldoutOffset() {
+		return new Vector2(0f, -4f);
+	}
+
+	public override void SetDefaults() {
+		Item.DefaultToCanisterUsingWeapon(50, 50, 10f, 36, 4f);
+		Item.width = 56;
+		Item.height = 18;
+		Item.SetShopValues(ItemRarityColor.Lime7, Item.sellPrice(gold: 7));
+		// TODO: revisit sound
+		Item.UseSound = SoundID.Item10 with { PitchRange = (-1f, -0.8f) };
+	}
+
+	public override void ApplyWeaponStats(ref CanisterShootStats stats) {
+		stats.ProjectileCount *= 3;
+		stats.TotalSpread += 0.2f;
+	}
+	
+	// TODO: acquisition
+}
